d6ba220fb4abaeb1146b7bd6321d008c0bdee1fd
bcadot <55708764+bcadot@users.noreply.github.com>
2021-12-23 11:11:25 +0100

    Init

---
+++ src/main/java/fr/smartcameras/cadot/smartcamerams/SmartcameramsApplication.java
@@ -0,0 +1,13 @@
+package fr.smartcameras.cadot.smartcamerams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcameramsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartcameramsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/smartcameras/cadot/smartcamerams/SmartcameramsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.smartcameras.cadot.smartcamerams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcameramsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
