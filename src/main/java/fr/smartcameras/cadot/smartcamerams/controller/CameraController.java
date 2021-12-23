04a8359fe1b7cc42d417cea3b21a082b00ecac28
bcadot <55708764+bcadot@users.noreply.github.com>
2021-12-23 11:28:36 +0100

    Controller creation

---
+++ src/main/java/fr/smartcameras/cadot/smartcamerams/controller/CameraController.java
@@ -0,0 +1,8 @@
+package fr.smartcameras.cadot.smartcamerams.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CameraController {
+    //TODO
+}
